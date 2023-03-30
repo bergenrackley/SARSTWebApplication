c1fe8b93bd990c35ad01da6ff413a0db0e206748
Casey Jacuzzi <62916413+ccjacuzzi@users.noreply.github.com>
2023-03-30 12:09:15 -0500

    Working on the registration request. I added some comments to the LoginController.

--- SARSTWebApplication/Controllers/LoginController.cs
+++ SARSTWebApplication/Controllers/LoginController.cs
@@ -12,6 +12,10 @@ namespace SARSTWebApplication.Controllers
         {
             _dbContext = new AppDbContext("Server=tcp:sarst.database.windows.net,1433;Initial Catalog=sarst;Persist Security Info=False;User ID=sarstadmin;Password=S4rstP4ssw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
+        
+        // GET: /Login
+        // Landing Page 
+        // Right now it is displaying a list of users from the database
         public IActionResult Index()
         {
             var viewModel = new LoginViewModel();
@@ -19,19 +23,28 @@ namespace SARSTWebApplication.Controllers
             return View(viewModel);
         }
 
+        // GET: /Login/Register
+        // Submit Registration Request
         public IActionResult Register()
         {
             return View("Register");
         }
-
+        
+        // SubmitRegistrationRequest        
         [HttpPost]
         public string submitRegistration(string userName, string firstName, string lastName, string email, string password, string userRole)
         {
+            // Check for duplicate
             SarstUser existingUser = _dbContext.Users.Find(userName);
             if (existingUser != null) { return "user already exists"; }
-            else return "new user";
+            else return "call AddUser method to add the request to the queue"; 
+
+            // If no duplicate, add user request information to registration request queue
         }
 
+        //TODO1: Add registration request queue model and update the DB to create table
+        //TODO2: Update this method to store all values from the request form in the request queue
+      
         public string AddUser(string uName, string pword)
         {
             var newRow = new SarstUser
