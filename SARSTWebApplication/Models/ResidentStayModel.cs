62100fcfbcb320aed341a0adc4f78fd801962279
wanickols01 <wanickols@ualr.edu>
2023-03-30 10:04:27 -0500

    ResidentStay / Service
    Quick draft for models of service and resident stay

---
+++ SARSTWebApplication/Models/ResidentStayModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SARSTWebApplication.Models
+{
+    public class ResidentStayModel
+    {
+
+        [Key]
+        public ResidentProfile forResident { get; set; }
+        public int checkoutDateTime { get; set; }
+        public UserProfile provider { get; set; }
+
+        public ResidentStayModel() 
+        {
+            forResident = new ResidentProfile(); // Resident 
+            checkoutDateTime = 0;
+            provider = new UserProfile(); // who checks them out
+        }
+    }
+}
