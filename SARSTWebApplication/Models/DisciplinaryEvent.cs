852defd608b9d9455995334b0c79ad8c4b728a1c
wanickols01 <wanickols@ualr.edu>
2023-04-06 01:00:19 -0500

    Create DisciplinaryEvent.cs
    Added Disciplinary Action Event with parameters from rfp

---
+++ SARSTWebApplication/Models/DisciplinaryEvent.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SARSTWebApplication.Models
+{
+    public enum disciplinaryActionType
+    {
+        Warning,
+        Education,
+        LastChanceContact,
+        Step_Away
+    }
+
+    public class DisciplinaryEvent
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int disciplinaryEventId { get; set; }
+
+        [ForeignKey("ResidentStay")]
+        public int residentStayId { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime dateIssued { get; set; }
+
+        public disciplinaryActionType typeOfDisciplinaryAction { get; set; }
+
+        public string reasonGiven { get; set; }
+        public DisciplinaryEvent()
+        {
+            typeOfDisciplinaryAction = disciplinaryActionType.Warning;
+            disciplinaryEventId = 0;
+            residentStayId = 0;
+            dateIssued = DateTime.MinValue;
+        }
+    }
+}
