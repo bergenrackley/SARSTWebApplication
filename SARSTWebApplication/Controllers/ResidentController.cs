6b3711176a87ed59d6a17f09323fe024e589fc0f
wanickols01 <wanickols@ualr.edu>
2023-04-22 17:25:28 -0500

    Edit and Delete for Residents
    Added edit for residents
    Added delete for residents
    Both Functionality and pages

--- SARSTWebApplication/Controllers/ResidentController.cs
+++ SARSTWebApplication/Controllers/ResidentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using SARSTWebApplication.Data;
 using SARSTWebApplication.Models;
 using System.Data.Entity.Infrastructure;
@@ -106,5 +107,101 @@ namespace SARSTWebApplication.Controllers
             }
         }
 
+
+        // GET: ResidentStays/Edit/5
+        public async Task<IActionResult> Edit(string? id)
+        {
+            if (id.IsNullOrEmpty())
+                return NotFound();
+
+            var resident = _dbContext.Residents.Find(id);
+
+            if (resident == null)
+                return NotFound();
+
+            return View(resident);
+        }
+
+        // POST: ResidentStays/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("residentId,firstName,lastName,dateOfBirth,sex, gender, pronouns, distinguishingFeatures, status")] Resident resident)
+        {
+            if (id != resident.residentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Find the existing entity by its primary key
+                    var existingResident = _dbContext.Residents.Find(id);
+
+                    // Assign the new values to its properties
+                    existingResident.residentId = resident.residentId;
+                    existingResident.firstName = resident.firstName;
+                    existingResident.lastName = resident.lastName;
+                    existingResident.sex = resident.sex;
+                    existingResident.gender = resident.gender;
+                    existingResident.pronouns = resident.pronouns;
+                    existingResident.distinguishingFeatures = resident.distinguishingFeatures;
+                    existingResident.status = resident.status;
+
+                    // Save the changes
+                    await _dbContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (_dbContext.ResidentStays.Find(resident.residentId) == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(resident);
+        }
+
+        // GET: ResidentStays/Delete/5
+        public async Task<IActionResult> Delete(string? id)
+        {
+            if (id.IsNullOrEmpty())
+                return NotFound();
+
+            var resident = _dbContext.Residents.Find(id);
+
+            if (resident == null)
+                return NotFound();
+
+            return View(resident);
+        }
+
+        // POST: ResidentStays/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirme(string id)
+        {
+
+            var resident = _dbContext.Residents.Find(id);
+
+            if (resident == null)
+            {
+                return Problem("Entity set 'SARSTWebApplicationContext.Resident'  is null.");
+            }
+            else
+                _dbContext.Residents.Remove(resident);
+
+
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
