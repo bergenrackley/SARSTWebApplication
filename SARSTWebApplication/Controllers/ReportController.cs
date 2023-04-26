8d0763664b70907f484701f726541308bcb43819
bergenrackley <bergenrackley@gmail.com>
2023-04-26 15:55:13 -0500

    Starting Report Controller

---
+++ SARSTWebApplication/Controllers/ReportController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+using SARSTWebApplication.Data;
+using SARSTWebApplication.Models;
+using System.Data.SqlClient;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace SARSTWebApplication.Controllers
+{
+    public class ReportController : BaseController
+    {
+        private AppDbContext _dbContext;
+
+        public ReportController(IConfiguration configuration)
+        {
+            _dbContext = new AppDbContext(configuration.GetConnectionString("DefaultConnection"));
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string GenerateReport(ReportModel reportModel)
+        {
+            
+            List<ResidentStay> result = _dbContext.Database.SqlQuery<ResidentStay>("Select * from dbo.residentStays where CheckInDateTime > @start", new SqlParameter("@start", reportModel.startDate ?? DateTime.MinValue)).ToList();
+            return result.ToJson();
+        }
+    }
+}
---
+++ SARSTWebApplication/Models/ReportModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SARSTWebApplication.Models
+{
+    public class ReportModel
+    {
+        [DataType(DataType.Date)]
+        [Display(Name = "Report Start Date")]
+        public DateTime? startDate { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Report End Date")]
+        public DateTime? endDate { get; set; }
+
+        public ReportModel()
+        {
+            startDate = null;
+            endDate = null;
+        }
+    }
+}
